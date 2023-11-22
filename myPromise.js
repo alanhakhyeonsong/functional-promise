60bcff82378d6075ecfc0607d424f523faa5d089
alanhakhyeonsong <songs4805@naver.com>
2023-11-22 23:55:18 +0900

    Feat: 초기 입출력 예시 추가

--- myPromise.js
+++ myPromise.js
@@ -12,3 +12,18 @@ promise.then((value) => {
 });
 
 console.log("Promise:", 6);
+
+
+// 직접 만든 Fromise9 출력
+console.log("Fromise9:", 1);
+let fromise9 = new Fromise9((resolve, reject) => {
+  console.log("Fromise9:", 2);
+  // throw new Error("에러야");
+  resolve(3);
+  console.log("Fromise9:", 4);
+});
+console.log("Fromise9:", 5);
+fromise9.then((value) => {
+  console.log("Fromise9:", "then1", value);
+})
+console.log("Fromise9:", 6);
