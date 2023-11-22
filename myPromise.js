5484f7d6fdef92dc1c20d1a5b6775d53552e7268
alanhakhyeonsong <songs4805@naver.com>
2023-11-23 00:14:34 +0900

    Feat: catch 예제 추가

--- myPromise.js
+++ myPromise.js
@@ -34,8 +34,9 @@ let promise = new Promise((resolve, reject) => {
 console.log("Promise:", 5);
 promise.then((value) => {
   console.log("Promise:", "then1", value);
+}).catch(error => {
+  console.error("Promise:", "error", error);
 });
-
 console.log("Promise:", 6);
 
 
@@ -44,12 +45,14 @@ console.log("Fromise9:", 1);
 let fromise9 = new Fromise9((resolve, reject) => {
   console.log("Fromise9:", 2);
   // throw new Error("에러야");
-  // resolve(3);
-  reject(3);
+  resolve(3);
+  // reject(3);
   console.log("Fromise9:", 4);
 });
 console.log("Fromise9:", 5);
 fromise9.then((value) => {
   console.log("Fromise9:", "then1", value);
-})
+}).catch(error => {
+  console.error("Fromise9", "error", error);
+});
 console.log("Fromise9:", 6);
