1132909fcc4df160a9da63a63d490db72b7c37a3
alanhakhyeonsong <songs4805@naver.com>
2023-11-23 00:38:04 +0900

    Feat: Promise 체이닝 예제 추가

--- myPromise.js
+++ myPromise.js
@@ -39,13 +39,15 @@ console.log("Promise:", 1);
 let promise = new Promise((resolve, reject) => {
   console.log("Promise:", 2);
   //throw new Error('에러야');
-  // resolve(3);
-  reject(3);
+  resolve(3);
+  // reject(3);
   console.log("Promise:", 4);
 });
 console.log("Promise:", 5);
 promise.then((value) => {
   console.log("Promise:", "then1", value);
+}).then((value) => {
+  console.log("Promise:", "then2", value);
 }).catch(error => {
   console.error("Promise:", "error", error);
 });
@@ -57,13 +59,15 @@ console.log("Fromise9:", 1);
 let fromise9 = new Fromise9((resolve, reject) => {
   console.log("Fromise9:", 2);
   // throw new Error("에러야");
-  // resolve(3);
-  reject(3);
+  resolve(3);
+  // reject(3);
   console.log("Fromise9:", 4);
 });
 console.log("Fromise9:", 5);
 fromise9.then((value) => {
   console.log("Fromise9:", "then1", value);
+}).then((value) => {
+  console.log("Fromise9:", "then2", value);
 }).catch(error => {
   console.error("Fromise9", "error", error);
 });
